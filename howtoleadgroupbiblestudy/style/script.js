b5119bbb8a0fc72548445100d10b1ce647377906
James Musson <jtmusson@hey.com>
2024-04-29 07:53:12 +0100

    Add files via upload

---
+++ howtoleadgroupbiblestudy/style/script.js
@@ -0,0 +1,6 @@
+document.querySelector(".hamburger-menu").addEventListener("click", () => {
+    document.querySelector(".container").classList.toggle("change");
+  });
+document.querySelector(".btn").addEventListener("click", () => {
+    document.querySelector(".container").classList.toggle("change");
+  });
