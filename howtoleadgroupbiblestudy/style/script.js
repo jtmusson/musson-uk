456a6685a84d6259ed76b09eef23264ba3b6d52c
James Musson <jtmusson@hey.com>
2024-05-06 15:03:44 +0100

    Remove HTTTB and HTLGBS

--- howtoleadgroupbiblestudy/style/script.js
+++
@@ -1,6 +0,0 @@
-document.querySelector(".hamburger-menu").addEventListener("click", () => {
-    document.querySelector(".container").classList.toggle("change");
-  });
-document.querySelector(".btn").addEventListener("click", () => {
-    document.querySelector(".container").classList.toggle("change");
-  });
